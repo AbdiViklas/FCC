84af7ff4009a6198b38952ef002eb8300959484f
Andy Bonner <abonner@duke.edu>
2016-06-28 16:50:11 -0400

    Update romanNumeral.js

--- romanNumeral.js
+++ romanNumeral.js
@@ -1,5 +1,13 @@
 function convertToRoman(num) {
- return num;
+ var digits = {
+   1: "I";
+   5: "V";
+   10: "X";
+   50: "L";
+   100: "C";
+   500: "D";
+   1000: "M";
+ }
 }
 
 convertToRoman(36);
